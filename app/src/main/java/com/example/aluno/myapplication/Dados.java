b57d4a5c57585e9a5bc5187e5933d2bc93e5ce59
aluno <felipekdds2011@gmail.com>
2019-08-12 11:23:23 -0300

    bora la

--- app/src/main/java/com/example/aluno/myapplication/Dados.java
+++ app/src/main/java/com/example/aluno/myapplication/Dados.java
@@ -2,8 +2,13 @@ package com.example.aluno.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Dados extends AppCompatActivity {
+    private TextView dis;
+    private TextView pas;
+    private TextView velm;
+    private TextView ritm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/aluno/myapplication/MainActivity.java
+++ app/src/main/java/com/example/aluno/myapplication/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.aluno.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.google.android.gms.maps.MapView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView crono;
+    private ToggleButton onoff;
+    private Button stop;
+    private MapView mapa;
+    private Chronometer cronome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapView mapa = (MapView) findViewById(R.id.mapa) ;
+        mapa.setClickable(true) ;
+
+       /* MyLocationOverlay
+        MyLocation
+        MyLocationOverlay mlo = new MyLocationOverlay(this, mapView) ;
+        mlo.enableCompass() ;
+        mlo.enableMyLocation() ;
+        mapView.getOverlays().add(mlo) ;*/
     }
 }
